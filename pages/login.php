d524dd258158cb10ba084d8d121a9a0d79f4c7f6
root <root@DESKTOP-LF5CPSS.localdomain>
2023-02-11 11:04:29 -0300

    Conclusao login

---
+++ pages/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <title>Login</title>
+</head>
+<body>
+    <main class="container">
+        <fieldset class="form-container">
+            <img src="../assets/icons8-user-circle-96.png" alt="">
+            <h1>Iniciar sessão</h1>
+            <form action="POST" id="form" class="form">
+                   
+                    <input type="email" class="input" placeholder="Email">
+                    <div class="line"></div>
+                    <input type="password" class="input" placeholder="Senha">
+                    <div class="line"></div>
+
+                <button type="submit" class="submit">Entrar</button>
+            </form>
+        </fieldset>
+        
+    </main>
+</body>
+</html>
